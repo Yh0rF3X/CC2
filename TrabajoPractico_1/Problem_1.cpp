50d58d9d404e814353a852a4f15040ccd228b54a
Yh0rF3X <38121451+Yh0rF3X@users.noreply.github.com>
2018-04-05 22:49:52 -0500

    Update Problem_1.cpp

--- TrabajoPractico_1/Problem_1.cpp
+++ TrabajoPractico_1/Problem_1.cpp
@@ -1,1 +1,22 @@
-
+    //intercambio de valores por paso por valor
+    int intercambiarValores(int x,int y){
+      int auxiliar;
+      auxiliar = v1;
+      v1 = v2;
+      v2 = auxiliar;
+    }
+    
+    //Intercambio de vañlores de paso por referencia
+    int intercambiarValores(int *x,int *y){
+      int aux;
+      aux =*x;
+      *x=*b;
+      *b=aux;
+    }
+void main(){
+  int a,b;
+  a=2:
+  b=3;
+  intecambiarValores(a, b);
+  intecambiarValores(&a, &b);
+}
